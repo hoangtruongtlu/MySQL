3b2c6ff7307ac378d4d12fcbe3ac5b8e1510253b
Trường <36031578+hoangtruongtlu@users.noreply.github.com>
2020-08-30 23:27:26 +0700

    Add files via upload

---
+++ Assignment4.sql
@@ -0,0 +1,27 @@
+USE classicmodels;
+
+SELECT  * FROM Orders;
+SELECT  * FROM customers;
+SELECT * FROM OrderDetails;
+SELECT * FROM Products;
+
+-- Tổng giá trị các hóa đơn đã xuất kho trong tháng 8-2004
+SELECT SUM(priceEach*quantityOrdered)
+FROM Orders o
+JOIN OrderDetails od ON o.orderNumber = od.orderNumber
+WHERE o.`status`='Shipped' AND o.OrderDate BETWEEN '2004-08-01' AND '2004-08-31';
+
+-- Lợi nhuận thu được từ mỗi khách hàng 
+SELECT c.customerName, SUM(od.quantityOrdered*od.priceEach - p.buyPrice*od.quantityOrdered) Profit
+FROM orderdetails od
+JOIN orders o 		ON od.orderNumber = o.orderNumber
+JOIN products p 	ON od.productCode = p.productCode
+JOIN customers c 	ON o.customerNumber = c.CustomerNumber
+GROUP BY o.CustomerNumber
+ORDER BY c.CustomerName;
+
+-- Doanh thu mà mỗi sản phẩm mang lại
+SELECT p.productCode,p.productName, SUM(od.quantityOrdered*od.priceEach) Revenue
+FROM products p
+JOIN orderdetails od	ON p.productCode = od.productCode
+GROUP BY p.productCode;
